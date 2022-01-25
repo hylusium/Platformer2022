1176ffb51150758385324478151c5908a64a41d7
hugo <hugo.drevet@hotmail.Fr>
2022-01-25 13:42:12 +0000

    [dev] clear console error

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Utilities/RewindTime.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Utilities/RewindTime.cs
@@ -10,14 +10,21 @@ namespace GSGD2.Utilities
         #region variables
 
         [SerializeField]
-        private int _arrayLength = 0;
+        private int _arrayLength = 5;
+
 
         [SerializeField]
-        private Transform[] _debugArray = null;
-        // Transform Array for Rewind Get a value Of 5
-        //
+        private float _duration = 0;
 
 
+        [SerializeField]
+        private List<Transform> _debugArray = null;
+        private bool coroutinesOver = true;
+        private int _arrayMinusOne;
+
+        [SerializeField] private float _rewindSpeed = 5f;
+
+        
 
 
         #endregion variables
@@ -25,33 +32,66 @@ namespace GSGD2.Utilities
 
         private void Start()
         {
-            Transform[] _lastknownLocation = new Transform[_arrayLength];
+            // List<Part> parts = new List<Part>();
+            List<Transform> _lastKnownLocation = new List<Transform>();
+            UpdatePos(_lastKnownLocation);
+            _arrayMinusOne = _arrayLength - 1;
 
-            UpdatePos(_lastknownLocation);
-            
+
         }
 
 
-        public Transform[] UpdatePos(Transform[] Vector)
+        public List<Transform> UpdatePos(List<Transform> Vector)
         {
 
+            Debug.Log("Ouais la zone");
+            if (Vector.Count >= _arrayLength)
+            {
+                Debug.Log("Test");
+                Vector.RemoveAt(4);
+                Vector.Add(this.transform);
+            }
+            else
+            {
+                Vector.Add(this.transform);
+            }
+            coroutinesOver = true;
             _debugArray = Vector;
-            return Vector;
+            return _debugArray;
         }
 
 
 
         private void Update()
         {
-            
+
+            AddTransformToList();
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                RewindAction();
+            }
+
         }
 
         private void AddTransformToList()
         {
 
+            StartCoroutine(PositionCD(_duration));
 
 
 
+        }
+
+        IEnumerator PositionCD(float duration)
+        {
+            if (coroutinesOver == true)
+            {
+                coroutinesOver = false;
+                yield return new WaitForSeconds(duration);
+                UpdatePos(_debugArray);
+
+
+            }
         }
 
 
@@ -59,15 +99,32 @@ namespace GSGD2.Utilities
 
 
 
+        private void RewindAction()
+        {
+
+            _debugArray.Reverse();
+            //foreach (Transform item in _debugArray)
+            //{
+
+
+
+            //    transform.position += item.position;
+            //        Debug.Log("lerp");
+
+
+            //}
+            //boucle for si ça marche pas 
+               
+            for (int i = 0; i < _debugArray.Count ; i++)
+            {
+            }
+            _debugArray.Clear();
+
+        }
 
 
 
     }
-
-
-
-
-
 
 }
 
