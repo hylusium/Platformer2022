6eaae6ca467fc6fcf07d4e16c73b6a181dd8da9f
Hugo <hugo.drevet@hotmail.fr>
2022-01-28 16:05:11 +0100

    [dev] commit change

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Utilities/RewindTime.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Utilities/RewindTime.cs
@@ -9,28 +9,16 @@ namespace GSGD2.Utilities
 
         #region variables
 
-        [SerializeField]
-        private int _arrayLength = 5;
+        [SerializeField] private int _arrayLength = 5;
+        [SerializeField] private float _duration = 0;
+        [SerializeField] private List<Vector3> _debugArray = null;
+       
 
-
-        [SerializeField]
-        private float _duration = 0;
-        
-
-
-
-        [SerializeField]
-        private List<Vector3> _debugArray = null;
         private bool coroutinesOver = true;
         private int _arrayMinusOne;
         private int _arrayIndex = 0;
 
-
-
-
-
         #endregion variables
-
 
         private void Start()
         {
@@ -38,14 +26,10 @@ namespace GSGD2.Utilities
             List<Vector3> _lastKnownLocation = new List<Vector3>();
             UpdatePos(_lastKnownLocation);
             _arrayMinusOne = _arrayLength - 1;
-
-
         }
-
 
         public List<Vector3> UpdatePos(List<Vector3> Vector)
         {
-
             Debug.Log("Ouais la zone");
             if (Vector.Count >= _arrayLength)
             {
@@ -62,26 +46,18 @@ namespace GSGD2.Utilities
             return _debugArray;
         }
 
-
-
         private void Update()
         {
-
             AddTransformToList();
             if (Input.GetKeyDown(KeyCode.B))
             {
                 RewindAction();
             }
-
         }
 
         private void AddTransformToList()
         {
-
             StartCoroutine(PositionCD(_duration));
-
-
-
         }
 
         IEnumerator PositionCD(float duration)
@@ -91,50 +67,38 @@ namespace GSGD2.Utilities
                 coroutinesOver = false;
                 yield return new WaitForSeconds(duration);
                 UpdatePos(_debugArray);
-
-
             }
         }
-
-        
-
-
-
 
         private void RewindAction()
         {
             Vector3[] tempTransform = _debugArray.ToArray();
+
             //for (int i = 0; i < _arrayMinusOne; i++)
             //{
 
-               
-                
-                
-                
             //}
+
+            for (int i = 0; i < tempTransform.Length; i++)
+            {
+                Debug.LogFormat("{0}, : valeur du tableau", tempTransform.GetValue(i));
+            }
             foreach (Vector3 item in tempTransform)
             {
                 if (_arrayIndex < _arrayMinusOne)
                 {
-                    MoveToNextWaypoint(tempTransform[_arrayIndex]);
+                    MoveToNextWaypoint(item);
                     _arrayIndex++;
                 }
             }
-            for (int i = 0; i < _arrayMinusOne; i++)
-            {
-                tempTransform = null;
-            }
-            
+            //tempTransform = null;
             _debugArray.Clear();
-
         }
-
 
         private void MoveToNextWaypoint(Vector3 nextWaypoint)
         {
             // Find Direction : direction = targetPosition - selfPosition;
             transform.position += (nextWaypoint - transform.position);
-            
         }
     }
 }
