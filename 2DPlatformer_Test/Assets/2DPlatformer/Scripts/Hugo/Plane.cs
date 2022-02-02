cee6b270a7bc8c2aff4ee209e11b7251a29a9fb6
Hugo <hugo.drevet@hotmail.fr>
2022-02-02 09:54:10 +0100

    [DEV] add the plane for screen when u leave the scene

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Hugo/Plane.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Hugo/Plane.cs
@@ -5,19 +5,34 @@ using GSGD2.Player;
 
 public class Plane : MonoBehaviour
 {
-    [SerializeField] private Transform _instantiatePoint = null;
     [SerializeField] private GameObject _plane = null;
     [SerializeField] private CubeController _playerRef = null;
+    [SerializeField] private bool _isItFirst = false;
+
+    private void Awake()
+    {
+        if (_isItFirst == true)
+        {
+            _plane.SetActive(false);
+
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponentInParent<CubeController>())
         {
-            Instantiate(_plane, _instantiatePoint);
+            _plane.SetActive(true);
         }
     }
 
-
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponentInParent<CubeController>())
+        {
+            _plane.SetActive(false);
+        }
+    }
 
 
 
